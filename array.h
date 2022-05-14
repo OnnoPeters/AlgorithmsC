16833e5b836963e3006c1f7e63be692cd31e33c0
OnnoPeters <opeters3@smail.uni-koeln.de>
2022-05-14 15:41:40 +0200

    Add new useful array methods

---
+++ array.h
@@ -0,0 +1,87 @@
+#include <malloc.h>
+
+void printArray(int arr[], int numElements)
+{
+    printf("\nArray: \n");
+    for(int i = 0; i < numElements; i++)
+    {
+        printf("%d \n", arr[i]);
+    }
+}
+
+void mergeArrays(int arr[], int leftIndex, int middleIndex, int rightIndex)
+{
+    int leftArrayLength = middleIndex - leftIndex + 1;
+    int rightArrayLength = rightIndex - middleIndex;
+    int tempLeftArray[leftArrayLength];
+    int tempRightArray[rightArrayLength];
+
+    for(int i = 0; i < leftArrayLength; i++)
+    {
+        tempLeftArray[i] = arr[leftIndex + i];
+    }
+    for(int i = 0; i < rightArrayLength; i++)
+    {
+        tempRightArray[i] = arr[middleIndex + 1 + i];
+    }
+
+    int leftCounter = 0;
+    int rightCounter = 0;
+    int counter = leftIndex;
+    while(leftCounter < leftArrayLength || rightCounter < rightArrayLength)
+    {
+        if(rightCounter == rightArrayLength)
+        {
+            arr[counter] = tempLeftArray[leftCounter];
+            counter++;
+            leftCounter++;
+            continue;
+        }
+        else if(leftCounter == leftArrayLength)
+        {
+            arr[counter] = tempRightArray[rightCounter];
+            counter++;
+            rightCounter++;
+            continue;
+        }
+
+        if(tempLeftArray[leftCounter] <= tempRightArray[rightCounter])
+        {
+            arr[counter] = tempLeftArray[leftCounter];
+            leftCounter++;
+        }
+        else
+        {
+            arr[counter] = tempRightArray[rightCounter];
+            rightCounter++;
+        }
+        counter++;
+    }
+}
+
+void mergeSort(int arr[], int leftIndex, int rightIndex)
+{
+    if(leftIndex < rightIndex)
+    {
+        int middleIndex = (int) (leftIndex + rightIndex) / 2;
+        mergeSort(arr, leftIndex, middleIndex);
+        mergeSort(arr, middleIndex + 1, rightIndex);
+
+        mergeArrays(arr, leftIndex, middleIndex, rightIndex);
+    }
+}
+
+void reverse(int arr[], int numElements)
+{
+    int half = (int) numElements / 2;
+    int tempHalfArray[half];
+    for(int i = 0; i < half; i++)
+    {
+        tempHalfArray[i] = arr[i];
+        arr[i] = arr[numElements - (i + 1)];
+    }
+    for(int i = 0; i < half; i++)
+    {
+        arr[numElements - (i + 1)] = tempHalfArray[i];
+    }
+}
--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include "array.h"
 #include "binaryTree.h"
 #include "linkedList.h"
 #include "doubleLinkedList.h"
 
 int main() {
-    int arr[] = {1, 0, 2, 5, 7, 3, 6, 4};
-    TreeNode *root = createBinaryTreeFromArray(arr, sizeof(arr)/sizeof(arr[0]));
+    int arr[] = {3, 1, 2, 12, 4, 0};
+    int arrLength = sizeof(arr)/sizeof(arr[0]);
+    TreeNode *root = createBinaryTreeFromArray(arr, arrLength);
     printTree(root);
     int array[countNodes(root)];
     int counter = 0;
@@ -13,5 +15,12 @@ int main() {
     printf("\n%d\n", array[2]);
     printTree(root);
     destroyTree(root);
+
+    printArray(arr, arrLength);
+    mergeSort(arr, 0, arrLength - 1);
+    printArray(arr, arrLength);
+    reverse(arr, arrLength);
+    printArray(arr, arrLength);
+
     return 0;
 }
